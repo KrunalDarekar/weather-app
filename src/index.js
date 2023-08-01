10bf2c63acaf7008442e67d1fa1fea923f3a489b
Krunal <96249206+KrunalDarekar@users.noreply.github.com>
2023-08-01 15:24:35 +0530

    Remove the demo icon

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ import { getCurrentWeather, getForecastWeather } from "./weather-api-calls";
 import { processCurrentWeatherData, processForecastWeatherData } from "./process-data";
 import { displayCurrentWeatherData, displayForecastWeatherData } from "./display-data";
 
-const icon = document.querySelector(".icon-element");
-
-
 getCurrentWeather("london")
     .then((data) => {
         console.log(data);
         const requiredCurrentData = processCurrentWeatherData(data);
         console.log(requiredCurrentData);
-        icon.src = requiredCurrentData.icon;
         displayCurrentWeatherData(requiredCurrentData);
     });
 
