a3a97daf12e395e2c82f8b3a1729b53df1fe707e
Krunal <96249206+KrunalDarekar@users.noreply.github.com>
2023-08-01 16:53:43 +0530

    Refactor the way date and time is displayed

--- src/display-data.js
+++ src/display-data.js
@@ -1,6 +1,7 @@
 const displayCurrentWeatherData = (data) => {
     const location = document.querySelector(".location");
-    const localDateTime = document.querySelector(".local-date-time");
+    const localDayDate = document.querySelector(".local-day-date");
+    const localTime = document.querySelector(".local-time")
     const icon = document.querySelector(".current>.condition>.icon");
     const text = document.querySelector(".current>.condition>.text");
     const temperature = document.querySelector(".current>.condition>.temperature");
@@ -9,7 +10,14 @@ const displayCurrentWeatherData = (data) => {
     const windSpeed = document.querySelector(".wind-speed>.data");
 
     location.textContent = `${data.name}, ${data.country}`;
-    localDateTime.textContent = `${data.localTime.date}, ${data.localTime.time}`;
+    localDayDate.textContent = `${data.localTime.day}, ${data.localTime.date}`;
+    if (parseInt(data.localTime.time.slice(0, 2), 10) === 12) {
+        localTime.textContent = `${data.localTime.time} pm`
+    } else if (parseInt(data.localTime.time.slice(0, 2), 10) === 0) {
+        localTime.textContent = `12${data.localTime.time.slice(2)} am`
+    } else {
+        localTime.textContent = parseInt(data.localTime.time.slice(0, 2), 10) > 12 ? `${parseInt(data.localTime.time.slice(0, 2), 10) - 12}${data.localTime.time.slice(2)} pm` : `${data.localTime.time} am`;
+    }
     icon.src = data.icon;
     text.textContent = data.condition.text;
     temperature.textContent = `${data.temp_c} °C`;
@@ -19,24 +27,30 @@ const displayCurrentWeatherData = (data) => {
 }
 
 const displayForecastWeatherData = (data) => {
+    const plusOneDay = document.querySelector(".plus-one>.day");
     const plusOneIcon = document.querySelector(".plus-one>.icon");
     const plusOneText = document.querySelector(".plus-one>.text");
     const plusOneTemp = document.querySelector(".plus-one>.temperature");
+    const plusTwoDay = document.querySelector(".plus-two>.day")
     const plusTwoIcon = document.querySelector(".plus-two>.icon");
     const plusTwoText = document.querySelector(".plus-two>.text");
     const plusTwoTemp = document.querySelector(".plus-two>.temperature");
+    const plusThreeDay = document.querySelector(".plus-three>.day")
     const plusThreeIcon = document.querySelector(".plus-three>.icon");
     const plusThreeText = document.querySelector(".plus-three>.text");
     const plusThreeTemp = document.querySelector(".plus-three>.temperature");
 
+    plusOneDay.textContent = data.day1.day;
     plusOneIcon.src = data.day1.icon;
     plusOneText.textContent = data.day1.condition.text;
     plusOneTemp.textContent = `${data.day1.avgTemp_c}°C`;
 
+    plusTwoDay.textContent = data.day2.day;
     plusTwoIcon.src = data.day2.icon;
     plusTwoText.textContent = data.day2.condition.text;
     plusTwoTemp.textContent = `${data.day2.avgTemp_c}°C`;
 
+    plusThreeDay.textContent = data.day3.day;
     plusThreeIcon.src = data.day3.icon;
     plusThreeText.textContent = data.day3.condition.text;
     plusThreeTemp.textContent = `${data.day3.avgTemp_c}°C`;
--- src/process-data.js
+++ src/process-data.js
@@ -1,9 +1,11 @@
 const processLocalTime = (localtime) => {
     const splitArray = localtime.split(" ");
-    const nDate = new Date(splitArray[0]).toDateString();
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const ndate = new Date(splitArray[0]).toDateString().slice(4);
+    const nDay = days[new Date(splitArray[0]).getDay()];
     const nTime = splitArray[1];
 
-    const processedLocalTime = { date: nDate, time: nTime };
+    const processedLocalTime = { day: nDay, date: ndate, time: nTime };
 
     return processedLocalTime;
 }
@@ -35,25 +37,27 @@ const processCurrentWeatherData = (data) => {
 const processForecastWeatherData = (data) => {
     const requiredData = {};
 
+    const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
     requiredData.day1 = {};
     requiredData.day2 = {};
     requiredData.day3 = {};
 
     requiredData.day1.avgTemp_c = data.forecast.forecastday[0].day.avgtemp_c;
     requiredData.day1.avgTemp_f = data.forecast.forecastday[0].day.avgtemp_f;
-    requiredData.day1.date = new Date(data.forecast.forecastday[0].date).toDateString();
+    requiredData.day1.day = days[new Date(data.forecast.forecastday[0].date).getDay()];
     requiredData.day1.condition = data.forecast.forecastday[0].day.condition;
     requiredData.day1.icon = processIcon(data.forecast.forecastday[0].day.condition.icon);
 
     requiredData.day2.avgTemp_c = data.forecast.forecastday[1].day.avgtemp_c;
     requiredData.day2.avgTemp_f = data.forecast.forecastday[1].day.avgtemp_f;
-    requiredData.day2.date = new Date(data.forecast.forecastday[1].date).toDateString();
+    requiredData.day2.day = days[new Date(data.forecast.forecastday[1].date).getDay()];
     requiredData.day2.condition = data.forecast.forecastday[1].day.condition;
     requiredData.day2.icon = processIcon(data.forecast.forecastday[1].day.condition.icon);
 
     requiredData.day3.avgTemp_c = data.forecast.forecastday[2].day.avgtemp_c;
     requiredData.day3.avgTemp_f = data.forecast.forecastday[2].day.avgtemp_f;
-    requiredData.day3.date = new Date(data.forecast.forecastday[2].date).toDateString();
+    requiredData.day3.day = days[new Date(data.forecast.forecastday[2].date).getDay()];
     requiredData.day3.condition = data.forecast.forecastday[2].day.condition;
     requiredData.day3.icon = processIcon(data.forecast.forecastday[2].day.condition.icon);
 
